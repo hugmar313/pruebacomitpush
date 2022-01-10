4de1587c8fc3a1e1c9c0bcd56e6b6f1a819b40d7
hugmar <hugmar@lliurex-ALDA-CE>
2022-01-10 17:08:33 +0100

    Este si que deberia ser el repositorio adecuado

--- comitpush/src/comitpush/Comitpush.java
+++ comitpush/src/comitpush/Comitpush.java
@@ -15,22 +15,47 @@ public class Comitpush {
      */
     public static void main(String[] args) {
         Scanner input=new Scanner(System.in);
+        int minmuni,maxmuni;
+        maxmuni=0;
+        minmuni=0;
         int num;
         System.out.println("Introduzca el numero de temperaturas que vas a introducir:");
         num=input.nextInt();
-        double temp;
         double max=Double.MIN_VALUE;
         double min=Double.MAX_VALUE;
-        System.out.print("Introduce la/s temperaturas:");
-        for (int i = 1; i <= num; i++) {
-            temp = input.nextDouble();
-            if (temp < min) {
-                min = temp;
+        System.out.println("Introduce la/s temperaturas y de que municio/s son:");
+        int temp[]=new int[num];
+        String muni[]=new String[num];
+        for (int i=0;i<temp.length;i++){
+            System.out.println("Introduce la temperatura:");
+            temp[i]=input.nextInt();
+            if (temp[i] < 0) {
+                System.out.println("Es negativa.");
+            } else if (temp[i] > 0) {
+                System.out.println("Es positiva.");
+            } else {
+                System.out.println("Es 0.");
             }
-            if (temp > max) {
-                max=temp;
+            System.out.println("Y ahora de que municipio es:");
+            input.nextLine();
+            muni[i]= input.nextLine();    
+        }
+        
+        for (int i=0;i<temp.length;i++) {
+
+            if (temp[i] < min) {
+                min = temp[i];
+                minmuni=i;
+            }
+            if (temp[i] > max) {
+                max=temp[i];
+                maxmuni=i;
             }
         }   
-        System.out.println("La menor es "+min+" y la mayor es "+max+".");
+        System.out.println("Las temperaturas introducidas son:");
+        for (int i=0;i<temp.length;i++){
+            System.out.println(temp[i]);
+        }
+        System.out.println("La temperatura mayor es "+max+" de "+muni[maxmuni]+" y la menor es "+min+" de "+muni[minmuni]+".");
     }
 }
